38d1b0f1e787fc887b0d8936d73e99d23d74fc3e
ApliNi <Nm70727@qq.com>
2023-03-24 14:35:05 +0800

    移除不可配置的 "Formats a player's total reputation into a cool bar"

--- src/main/java/adhdmc/villagerinfo/VillagerHandling/ReputationHandler.java
+++ src/main/java/adhdmc/villagerinfo/VillagerHandling/ReputationHandler.java
@@ -10,32 +10,33 @@ public class ReputationHandler {
      * @return formatted reputation string
      */
     public static String villagerReputation(int repNum) {
-        StringBuilder output = new StringBuilder();
-        int minVal = -14;
-        int maxVal = 15;
-        int devNum = repNum / 50;
-        boolean isPos = Math.abs(devNum) == devNum;
-        for (int i = minVal; i <= maxVal; i++) {
-            if (i == 0) {
-                output.append(ChatColor.WHITE + "[").append(repNum).append("]");
-                continue;
-            }
-            if (Math.abs(i) != i) {
-                if (!isPos && i < 0 && i >= devNum) {
-                    output.append(ChatColor.RED + "▬");
-                    continue;
-                }
-                output.append(ChatColor.GRAY + "•");
-            }
-            if (Math.abs(i) == i) {
-                if (isPos && i > 0 && i <= devNum) {
-                    output.append(ChatColor.GREEN + "▬");
-                    continue;
-                }
-                output.append(ChatColor.GRAY + "•");
-            }
-        }
-        return output.toString();
+//        StringBuilder output = new StringBuilder();
+//        int minVal = -14;
+//        int maxVal = 15;
+//        int devNum = repNum / 50;
+//        boolean isPos = Math.abs(devNum) == devNum;
+//        for (int i = minVal; i <= maxVal; i++) {
+//            if (i == 0) {
+//                output.append(ChatColor.WHITE + "[").append(repNum).append("]");
+//                continue;
+//            }
+//            if (Math.abs(i) != i) {
+//                if (!isPos && i < 0 && i >= devNum) {
+//                    output.append(ChatColor.RED + "▬");
+//                    continue;
+//                }
+//                output.append(ChatColor.GRAY + "•");
+//            }
+//            if (Math.abs(i) == i) {
+//                if (isPos && i > 0 && i <= devNum) {
+//                    output.append(ChatColor.GREEN + "▬");
+//                    continue;
+//                }
+//                output.append(ChatColor.GRAY + "•");
+//            }
+//        }
+//        return output.toString();
+        return String.valueOf(repNum);
     }
 
 }
